68275375c2b90695d21a0b2acfab6993e139a538
Sally Poon <sallypoon@hotmail.com>
2022-10-02 12:38:49 -0700

    edit form label font

--- src/pages/newEvent.tsx
+++ src/pages/newEvent.tsx
@@ -12,7 +12,7 @@ function NewEvent() {
     const [showError, setShowError] = useState(false);
 
     useEffect(() => {
-        if (!CheckSession()) router.push('/login');
+        // if (!CheckSession()) router.push('/login');
     }, []);
 
     const submitForm = () => {
@@ -69,30 +69,30 @@ function NewEvent() {
                     transition={{ duration: .75 }}
                     className={"flex flex-col items-center border-[1px] border-black text-center font-fun p-5 rounded-2xl w-96"}>
 
-                    <label className="font-fun mt-2">Event Name</label>
+                    <label className="font-business mt-2">Event Name</label>
                         <input className={"input m-2 text-lg font-business text-center w-[100%]"}></input>
 
-                    <label className="font-fun mt-2">When</label>
+                    <label className="font-business mt-2">When</label>
                         <input className={"input m-2 text-lg font-business text-center w-[100%]"}></input>
 
-                    <label className="font-fun">Where</label>
+                    <label className="font-business mt-2">Where</label>
                         <input className={"input m-2 text-lg font-business text-center w-[100%]"}></input>
 
-                    <label className="font-fun mt-2">Locale</label>
+                    <label className="font-business mt-2">Locale</label>
                         <select className={"input m-2 font-business text-center w-[100%]"}>
                             <option value={"Americas"}>Americas</option>
                             <option value={"Asia"}>Asia</option>
                             <option value={"Europe"}>Europe</option>
                         </select>
 
-                    <label className="font-fun mt-2">Regionality</label>
+                    <label className="font-business mt-2">Regionality</label>
                         <select className={"input m-2 font-business text-center w-[100%]"}>
                             <option value={"Global"}>Global</option>
                             <option value={"Regional"}>Regional</option>
                             <option value={"Local"}>Local</option>
                         </select>
 
-                    <label className="font-fun mt-2">Description</label>
+                    <label className="font-business mt-2">Description</label>
                         <textarea 
                         className="input m-2 font-business text-center w-[100%]"></textarea>
 
