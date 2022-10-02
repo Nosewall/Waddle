4565c293fac9ddfdadd9161dc3b1d42c7a716643
MoBaydoun <mbaydoun@my.bcit.ca>
2022-10-02 12:40:42 -0700

    my features.

--- src/pages/newEvent.tsx
+++ src/pages/newEvent.tsx
@@ -1,16 +1,49 @@
 import HamburgerMenu from '../components/nav/hamburgerMenu';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '../components/context/AuthContext';
 import { motion } from 'framer-motion';
+import axiosBase from './axiosBase';
+
+const EventForm = Object.freeze({
+    scope: 'Global',
+    location: '',
+    time: '',
+    title: '',
+    body: '',
+    contactInfo: '',
+});
 
 function NewEvent() {
     const router = useRouter();
     const { CheckSession } = useAuth();
+    const [formData, updateFormData] = useState(EventForm);
 
     useEffect(() => {
         if (!CheckSession()) router.push('/login');
     }, []);
+
+    const handleChange = (e: any) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
+        });
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        const data = { ...formData };
+        axiosBase
+            .post('/create-event', data)
+            .then((res) => {
+                e.reset();
+                //toast
+            })
+            .catch((err) => {
+                //bad toast
+            });
+    };
+
     return (
         <>
             <HamburgerMenu />
@@ -21,70 +54,77 @@ function NewEvent() {
                 className={
                     'border-[1px] border-black text-center font-fun m-5'
                 }>
-                <h1 className={'p-2 text-3xl'}>Title</h1>
-                <div className={'m-2'}>
-                    <input
-                        className={
-                            'input m-2 text-2xl font-business text-center w-[70%]'
-                        }></input>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <h1 className={'p-2 text-3xl'}>Scope</h1>
+                    <div className={'m-2'}>
+                        <select
+                            className={
+                                'input m-2 text-2xl font-business text-center w-[70%]'
+                            }
+                            name={'scope'}
+                            onChange={handleChange}>
+                            <option value={'Global'}>Global</option>
+                            <option value={'Regional'}>Regional</option>
+                            <option value={'Local'}>Local</option>
+                        </select>
+                    </div>
 
-                <h1 className={'p-2 text-3xl'}>When</h1>
-                <div className={'m-2'}>
-                    <input
-                        className={
-                            'input m-2 text-2xl font-business text-center w-[70%] '
-                        }></input>
-                </div>
+                    <h1 className={'p-2 text-3xl'}>Title</h1>
+                    <div className={'m-2'}>
+                        <input
+                            className={
+                                'input m-2 text-2xl font-business text-center w-[70%]'
+                            }
+                            name={'title'}
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                <h1 className={'p-2 text-3xl'}>Where</h1>
-                <div className={'m-2'}>
-                    <input
-                        className={
-                            'input m-2 text-2xl font-business text-center w-[70%]'
-                        }></input>
-                </div>
+                    <h1 className={'p-2 text-3xl'}>When</h1>
+                    <div className={'m-2'}>
+                        <input
+                            className={
+                                'input m-2 text-2xl font-business text-center w-[70%] '
+                            }
+                            placeholder={'Format: YY-MM-DD hh:mm:ss'}
+                            name={'time'}
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                <h1 className={'p-2 text-3xl'}>Locale</h1>
-                <div className={'m-2'}>
-                    <select
-                        className={
-                            'input m-2 text-2xl font-business text-center w-[70%]'
-                        }>
-                        <option value={'Americas'}>Americas</option>
-                        <option value={'Asia'}>Asia</option>
-                        <option value={'Europe'}>Europe</option>
-                    </select>
-                </div>
+                    <h1 className={'p-2 text-3xl'}>Where</h1>
+                    <div className={'m-2'}>
+                        <input
+                            className={
+                                'input m-2 text-2xl font-business text-center w-[70%]'
+                            }
+                            name={'location'}
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                <h1 className={'p-2 text-3xl'}>Regionality</h1>
-                <div className={'m-2'}>
-                    <select
-                        className={
-                            'input m-2 text-2xl font-business text-center w-[70%]'
-                        }>
-                        <option value={'Global'}>Global</option>
-                        <option value={'Regional'}>Regional</option>
-                        <option value={'Local'}>Local</option>
-                    </select>
-                </div>
+                    <h1 className={'p-2 text-3xl'}>Description</h1>
+                    <div className={'m-2'}>
+                        <textarea
+                            className={
+                                'input m-2 text-2xl font-business text-center w-[70%]'
+                            }
+                            name={'body'}
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                <h1 className={'p-2 text-3xl'}>Description</h1>
-                <div className={'m-2'}>
-                    <textarea
+                    <button
                         className={
-                            'input m-2 text-2xl font-business text-center w-[70%]'
-                        }></textarea>
-                </div>
-
-                <button
-                    className={
-                        'button border-[1px] p-1 m-4 border-green-300 rounded-md ' +
-                        'bg-pastelGreen hover:bg-boldGreen w-[30%] ' +
-                        'transition-all duration-75'
-                    }>
-                    Go!{' '}
-                </button>
+                            'button border-[1px] p-1 m-4 border-green-300 rounded-md ' +
+                            'bg-pastelGreen hover:bg-boldGreen w-[30%] ' +
+                            'transition-all duration-75'
+                        }
+                        type={'submit'}
+                        name={'submit'}>
+                        Go!{' '}
+                    </button>
+                </form>
             </motion.div>
         </>
     );
