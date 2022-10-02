f9b9edfd9cced87fadab3713c5b1754776497178
Sally Poon <sallypoon@hotmail.com>
2022-10-02 12:28:58 -0700

    add event toasts

--- src/pages/newEvent.tsx
+++ src/pages/newEvent.tsx
@@ -1,21 +1,64 @@
 import HamburgerMenu from '../components/nav/hamburgerMenu';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '../components/context/AuthContext';
 import { motion } from 'framer-motion';
+import { Toast } from 'flowbite-react';
 
 function NewEvent() {
     const router = useRouter();
     const { CheckSession } = useAuth();
+    const [showSuccess, setShowSuccess] = useState(false);
+    const [showError, setShowError] = useState(false);
 
     useEffect(() => {
         if (!CheckSession()) router.push('/login');
     }, []);
-    
+
+    const submitForm = () => {
+        errorToast();
+    }
+
+    const successToast = () => {
+        setShowSuccess(true);
+        setTimeout(() => setShowSuccess(false), 3000);
+    }
+
+    const errorToast = () => {
+        setShowError(true);
+        setTimeout(() => setShowError(false), 3000);
+    }
+
     return (
         <>
             <HamburgerMenu />
             <div className='flex flex-col items-center page-container'>
+
+            {showSuccess && (
+                    <div className="fixed z-30">
+                        <Toast>
+                            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-boldPurple text-blue-500 dark:bg-blue-800 dark:text-blue-200">
+                                🎉
+                            </div>
+                            <div className="ml-3 text-sm font-normal">
+                               Event Added Successfully!
+                            </div>
+                        </Toast>
+                    </div>
+                )}
+
+                {showError && (
+                    <div className="fixed z-30">
+                        <Toast>
+                            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-500 text-blue-500 dark:bg-blue-800 dark:text-blue-200">
+                                🐻
+                            </div>
+                            <div className="ml-3 text-sm font-normal">
+                                Bad news bears! <br/> Something went wrong.
+                            </div>
+                        </Toast>
+                    </div>
+                )}
 
 
                 <p className='font-fun text-2xl my-6'>Add New Event</p>
@@ -53,9 +96,9 @@ function NewEvent() {
                         <textarea 
                         className="input m-2 font-business text-center w-[100%]"></textarea>
 
-                    <button className={"button border-[1px] p-1 m-4 border-green-300 rounded-md " +
-                        "bg-pastelGreen hover:bg-boldGreen w-[50%] " +
-                        "transition-all duration-75"}>Add Event! </button>
+                    <button className={"button border-[1px] p-1 m-4 rounded-md " +
+                        "bg-pastelTangerine scopeFilterButtons hover:bg-orange-500 w-[50%] " +
+                        "transition-all duration-75"} onClick={submitForm}>Add Event! </button>
 
                 </motion.div>
             </div>
