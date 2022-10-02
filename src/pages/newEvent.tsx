ceac23494ae0b0acd1b16648d40937728a15a0e7
Nolan Nordwall <NolanNordwall@gmail.com>
2022-10-02 09:42:41 -0700

    Added first animations. Pushing so I can send form fix to Mo

--- src/pages/newEvent.tsx
+++ src/pages/newEvent.tsx
@@ -1,10 +1,15 @@
 import HamburgerMenu from "../components/nav/hamburgerMenu";
+import {motion} from "framer-motion";
 
 function NewEvent() {
     return (
         <>
             <HamburgerMenu/>
-            <div className={"border-[1px] border-black text-center font-fun m-5"}>
+            <motion.div
+                animate={{opacity: 1, x:0}}
+                initial={{opacity:0, x:-200}}
+                transition={{duration: .75}}
+                className={"border-[1px] border-black text-center font-fun m-5"}>
 
                 <h1 className={"p-2 text-3xl"}>Title</h1>
                 <div className={"m-2"}>
@@ -19,6 +24,15 @@ function NewEvent() {
                 <h1 className={"p-2 text-3xl"}>Where</h1>
                 <div className={"m-2"}>
                     <input className={"input m-2 text-2xl font-business text-center w-[70%]"}></input>
+                </div>
+
+                <h1 className={"p-2 text-3xl"}>Locale</h1>
+                <div className={"m-2"}>
+                    <select className={"input m-2 text-2xl font-business text-center w-[70%]"}>
+                        <option value={"Americas"}>Americas</option>
+                        <option value={"Asia"}>Asia</option>
+                        <option value={"Europe"}>Europe</option>
+                    </select>
                 </div>
 
                 <h1 className={"p-2 text-3xl"}>Regionality</h1>
@@ -39,7 +53,7 @@ function NewEvent() {
                     "bg-pastelGreen hover:bg-boldGreen w-[30%] " +
                     "transition-all duration-75"}>Go! </button>
 
-            </div>
+            </motion.div>
         </>
     )
 }
